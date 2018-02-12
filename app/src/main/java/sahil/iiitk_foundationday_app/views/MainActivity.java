bade352c7981857701da22e88abf5c43310c06e7
Sahil Ayank <sahilayank91@gmail.com>
2018-02-12 21:57:38 +0530

    User Model Added

--- app/src/main/java/sahil/iiitk_foundationday_app/views/MainActivity.java
+++ app/src/main/java/sahil/iiitk_foundationday_app/views/MainActivity.java
@@ -1,7 +1,9 @@
-package sahil.iiitk_foundationday_app;
+package sahil.iiitk_foundationday_app.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import sahil.iiitk_foundationday_app.R;
 
 public class MainActivity extends AppCompatActivity {
 
